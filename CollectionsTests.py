960eedd3f577d52a7e13a0366fe7a2a807e671a3
Mattias Walla <mattias.walla@gmail.com>
2018-11-26 14:49:01 +0100

    working

--- CollectionsTests.py
+++ CollectionsTests.py
@@ -2,9 +2,10 @@ import unittest
 import collections
 
 
-class CollectionsTests(unittest.TestCase):
+class TestCollections(unittest.TestCase):
 
-    def order_and_len_test(self):
+    def test_order_and_len(self):
+        testData = collections.OrderedDict()
         for i in range(1000):
             testData[i] = i*i
             self.assertEqual(len(testData), i+1)
@@ -12,13 +13,13 @@ class CollectionsTests(unittest.TestCase):
             (key, value) = testData.popitem(False)
             self.assertEqual((key, value), (i, i*i))
 
-    def update_tests(self):
+    def test_update(self):
+        testData = collections.Counter()
         testData.update(test=3, test2=0, test3=-1)
         testData.update(test=3)
         self.assertEqual(testData['test'], 6)
         self.assertEqual(testData['test2'], 0)
         self.assertEqual(testData['test3'], -1)
-
 
 if __name__ == '__main__':
     unittest.main()
