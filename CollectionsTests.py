41dc0bec9b45be68a3e557a4a4d5ffae2927a0da
Mattias Walla <mattias.walla@gmail.com>
2018-11-26 14:31:08 +0100

    tests for counter

--- CollectionsTests.py
+++ CollectionsTests.py
@@ -5,13 +5,19 @@ import collections
 class CollectionsTests(unittest.TestCase):
 
     def order_and_len_test(self):
-        testData = collections.OrderedDict()
         for i in range(1000):
             testData[i] = i*i
             self.assertEqual(len(testData), i+1)
         for i in range(1000):
             (key, value) = testData.popitem(False)
             self.assertEqual((key, value), (i, i*i))
+
+    def update_tests(self):
+        testData.update(test=3, test2=0, test3=-1)
+        testData.update(test=3)
+        self.assertEqual(testData['test'], 6)
+        self.assertEqual(testData['test2'], 0)
+        self.assertEqual(testData['test3'], -1)
 
 
 if __name__ == '__main__':
